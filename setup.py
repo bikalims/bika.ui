b6aedacded119a918a64c3eb449e10efe4bd2fea
mikejmets <mike@webtide.co.za>
2021-05-27 17:43:49 +0200

    First attempt to customised icon and colours

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
     install_requires=[
         "setuptools",
         "senaite.lims",
+        "collective.localstyles",
     ],
     extras_require={
         "test": [
---
+++ src/bika/ui/setuphandlers.py
@@ -0,0 +1,26 @@
+from bika.ui import logger
+from bika.ui import PRODUCT_NAME
+from bika.ui import PROFILE_ID
+
+
+def post_install(portal_setup):
+    """Runs after the last import step of the *default* profile
+    This handler is registered as a *post_handler* in the generic setup profile
+    :param portal_setup: SetupTool
+    """
+    logger.info("{} post-install handler [BEGIN]".format(PRODUCT_NAME.upper()))
+    context = portal_setup._getImportContext(PROFILE_ID)
+    portal = context.getSite()  # noqa
+
+    logger.info("{} post-install handler [DONE]".format(PRODUCT_NAME.upper()))
+
+
+def install(context):
+    """Install handler
+    """
+    import pdb; pdb.set_trace()
+    if context.readDataFile("bika.ui.txt") is None:
+        return
+
+    logger.info("BIKA UI install handler [BEGIN]")
+    portal = context.getSite()
