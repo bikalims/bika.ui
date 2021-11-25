ec6414215f1d268667d9cb086c5e3b1a0ac7429c
Campbell <campbellmckellarbasset@gmail.com>
2021-11-25 17:39:11 +0200

    favicon

--- src/bika/ui/browser/bootstrap.py
+++ src/bika/ui/browser/bootstrap.py
@@ -1,26 +1,43 @@
-import os
+from bika.ui.setuphandlers import is_installed
+from senaite.core.browser.bootstrap import bootstrap
+from zope.interface import implementer
 
-from bika.lims import api
-from plone.memoize.ram import cache
-from senaite.core.browser.bootstrap.bootstrap import BootstrapView as BSV
-from senaite.core.browser.bootstrap.bootstrap import icon_cache_key
+IMG_STYLE = "width:24px; height:24px;"
 
 
-class BootstrapView(BSV):
+@implementer(bootstrap.IBootstrapView)
+class BootstrapView(bootstrap.BootstrapView):
 
-    @cache(icon_cache_key)
-    def get_icon_for(self, brain_or_object, **kw):
-        portal_types = api.get_tool("portal_types")
-        fti = portal_types.getTypeInfo(api.get_portal_type(brain_or_object))
-        icon = fti and fti.getIcon()  # handle non-type fti for bbb
-        if not icon:
-            return ""
-        title = api.get_title(brain_or_object)
-        # Always try to get the SVG icon for high-res displays
-        icon_basename = os.path.basename(icon)
-        path = '{}/++resource++bika.lims.images/{}.png'.format(
-            self.portal_state.portal_url(), icon_basename)
-        # self.img_tag(title=title, icon=icon_svg, **kw)
-        tag = "<img title='{}' src='{}' width='16' class='' />".format(
-            title, path)
-        return tag
+    def img_tag(self, title=None, icon=None, **kw):
+        if is_installed():
+            return super(BootstrapView, self).img_tag(
+                title=title, icon=icon, style=IMG_STYLE)
+        return super(BootstrapView, self).img_tag(
+            title=title, icon=icon, **kw)
+
+# import os
+#
+# from bika.lims import api
+# from plone.memoize.ram import cache
+# from senaite.core.browser.bootstrap.bootstrap import BootstrapView as BSV
+# from senaite.core.browser.bootstrap.bootstrap import icon_cache_key
+#
+#
+# class BootstrapView(BSV):
+#
+#     @cache(icon_cache_key)
+#     def get_icon_for(self, brain_or_object, **kw):
+#         portal_types = api.get_tool("portal_types")
+#         fti = portal_types.getTypeInfo(api.get_portal_type(brain_or_object))
+#         icon = fti and fti.getIcon()  # handle non-type fti for bbb
+#         if not icon:
+#             return ""
+#         title = api.get_title(brain_or_object)
+#         # Always try to get the SVG icon for high-res displays
+#         icon_basename = os.path.basename(icon)
+#         path = '{}/++resource++bika.lims.images/{}.png'.format(
+#             self.portal_state.portal_url(), icon_basename)
+#         # self.img_tag(title=title, icon=icon_svg, **kw)
+#         tag = "<img title='{}' src='{}' width='16' class='' />".format(
+#             title, path)
+#         return tag
