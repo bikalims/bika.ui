957d9a4b0646306d11e7d5f07737abd6fdab520d
Campbell <campbellmckellarbasset@gmail.com>
2021-12-04 15:44:27 +0200

    move toolbar.pt to jbot

--- src/bika/ui/browser/viewlets/toolbar.py
+++
@@ -1,6 +0,0 @@
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from senaite.core.browser.viewlets import toolbar
-
-
-class ToolbarViewletManager(toolbar.ToolbarViewletManager):
-    custom_template = ViewPageTemplateFile("templates/toolbar.pt")
